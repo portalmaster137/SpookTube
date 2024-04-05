ed8e3bde2eb479e468cb243605d5c642fc8a6837
porta <portalmaster137@gmail.com>
2024-04-06 04:59:46 +1000

    eee

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import https from 'https';
+import fs from 'fs';
+
+const app = express();
+const port = 443;
+
+app.route('/').get((req, res) => {
+    res.redirect('https://store.steampowered.com/app/2881650/Content_Warning/');
+});
+
+// Configure your routes and middleware here
+
+// Load SSL certificate and key
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/spooktu.be/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/spooktu.be/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/spooktu.be/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+// Create HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+// Start the server
+httpsServer.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
