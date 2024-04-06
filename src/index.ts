5261c45435c7a791d59ca9fc1a4eece8544ec276
porta <portalmaster137@gmail.com>
2024-04-06 15:16:38 +1000

    Add HTTPS redirection for non-secure requests

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ const port = 443;
 
 app.use(morgan('combined'));
 
+app.enable('trust proxy');
+
+app.use((req, res, next) => {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 app.route('/').get((req, res) => {
     res.redirect('https://store.steampowered.com/app/2881650/Content_Warning/');
 });
